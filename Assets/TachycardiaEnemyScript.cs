98e770eb72c7db48dce5e3ebf0dfff3b0f9ee0d5
BakersDozenBagels <Bakersdozenbagelsbergman@gmail.com>
2021-10-24 01:44:40 -0400

    [v1.0] Initial commit

---
+++ Assets/TachycardiaEnemyScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TachycardiaEnemyScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _transform;
+
+    public void SetMotion(float v)
+    {
+        float h = v + 0.07f * (-Mathf.Abs(v - 0.5f) + 0.5f) * Mathf.Sin(2f * Mathf.PI * v);
+        //float h = v;
+        float x = h + (-Mathf.Abs(h - 0.5f) + 0.5f) * Mathf.Sin(20f * Mathf.PI * h + Mathf.PI) / 16f / Mathf.PI;
+        float y = .6f * (-Mathf.Abs(x - 0.5f) + 0.5f) * Mathf.Sin(10f * Mathf.PI * x);
+        _transform.localPosition = new Vector3(x, 0, y);
+    }
+}
---
+++ Assets/TachycardiaScript.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TachycardiaScript : MonoBehaviour
+{
+    [SerializeField]
+    private KMSelectable _heart;
+    [SerializeField]
+    private Renderer _heartRenderer;
+    [SerializeField]
+    private Material _heartMat, _HLMat;
+    [SerializeField]
+    private TachycardiaEnemyScript _enemy;
+    [SerializeField]
+    private KMAudio _audio;
+    [SerializeField]
+    private KMBombModule _module;
+    [SerializeField]
+    private KMBombInfo _info;
+
+    private bool _defib = false, _isSolved = false;
+    private Coroutine _routine = null;
+    private float _lastHeld = -1f;
+    private static int _idc, _strikesToIgnore = 0;
+    private int _id = ++_idc, _strikes = 0;
+    private float _nextAttack = 90f;
+    private Action _onStrike = () => { };
+
+    private void Start()
+    {
+        Debug.LogFormat("[Tachycardia #{0}] Good luck. You'll need it...", _id);
+        _heart.OnHighlight += () => { _heartRenderer.material = _HLMat; };
+        _heart.OnHighlightEnded += () => { _heartRenderer.material = _heartMat; };
+
+        StartCoroutine(PlaySounds());
+        StartCoroutine(SendEnemies());
+
+        _heart.OnInteract += Hold;
+        _heart.OnInteractEnded += Release;
+    }
+
+    private IEnumerator SendEnemies()
+    {
+        yield return new WaitForSeconds(_nextAttack);
+        while(!_isSolved)
+        {
+            _routine = StartCoroutine(MoveEnemy());
+            _nextAttack = UnityEngine.Random.Range(8f, 12f) * _info.GetTime() / 60f;
+            yield return new WaitForSeconds(_nextAttack);
+        }
+    }
+
+    private void Update()
+    {
+        if(_info.GetStrikes() > _strikes && _strikesToIgnore == 0)
+            _onStrike();
+        _strikes = _info.GetStrikes();
+    }
+
+    private IEnumerator WaitShortly()
+    {
+        _strikesToIgnore = 1;
+        yield return null;
+        yield return null;
+        _strikesToIgnore = 0;
+    }
+
+    private void Release()
+    {
+        if(_lastHeld == -1f)
+            return;
+
+        if(Time.time - _lastHeld >= 5f)
+        {
+            Debug.LogFormat("[Tachycardia #{0}] Enter panic mode.", _id);
+            _module.HandlePass();
+            _audio.PlaySoundAtTransform("HeartBeat", _heart.transform);
+            _isSolved = true;
+            _nextAttack = float.PositiveInfinity;
+            _onStrike += () =>
+            {
+                StartCoroutine(WaitShortly());
+                _module.HandleStrike();
+                _module.HandleStrike();
+            };
+
+            _defib = false;
+            if(_routine != null)
+            {
+                StopCoroutine(_routine);
+                _routine = null;
+            }
+            _enemy.gameObject.SetActive(false);
+        }
+        _lastHeld = -1f;
+    }
+
+    private bool Hold()
+    {
+        _heart.AddInteractionPunch(1f);
+        _audio.PlaySoundAtTransform("Correct", _heart.transform);
+
+        if(_isSolved)
+            return false;
+
+        _lastHeld = Time.time;
+
+        if(!_defib)
+        {
+            StartCoroutine(WaitShortly());
+            _module.HandleStrike();
+            return false;
+        }
+
+        _defib = false;
+        if(_routine != null)
+        {
+            StopCoroutine(_routine);
+            _routine = null;
+        }
+        _enemy.gameObject.SetActive(false);
+
+        return false;
+    }
+
+    private IEnumerator PlaySounds()
+    {
+        yield return null;
+        while(true)
+        {
+            _audio.PlaySoundAtTransform("HeartMonitor", _heart.transform);
+            yield return new WaitForSeconds(60.995f);
+        }
+    }
+
+    private IEnumerator MoveEnemy()
+    {
+        const float DELAY = 7f;
+        _audio.PlaySoundAtTransform("HeartBeat2", _heart.transform);
+        _enemy.gameObject.SetActive(true);
+        float time = Time.time;
+        while(Time.time - time < DELAY)
+        {
+            _enemy.SetMotion((DELAY - Time.time + time) / DELAY);
+            if((DELAY - Time.time + time) / DELAY < 0.5f)
+                _defib = true;
+            yield return null;
+        }
+        _enemy.gameObject.SetActive(false);
+        if(_defib)
+        {
+            StartCoroutine(WaitShortly());
+            _module.HandleStrike();
+            _defib = false;
+        }
+    }
+}
